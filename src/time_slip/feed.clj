3ca33c76dcdbc5a8bf4619e8223073051dff1ef8
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-01-11 17:33:28 +0100

    Parse item fields

--- src/time_slip/feed.clj
+++ src/time_slip/feed.clj
@@ -1,17 +1,33 @@
 (ns time-slip.feed
   (:require [clojure.xml :as xml]
             [clojure.zip :as zip]
-            [clojure.data.zip.xml :as zip-xml]))
+            [clojure.data.zip.xml :as zip-xml]
+            [clojure.string :as string]
+            [clj-time.format :as format]
+            [clj-time.core :as time]))
 
 (defn- parse-str [data]
   (xml/parse (java.io.ByteArrayInputStream. (.getBytes data))))
 
-(defn parse
-  [data]
-  (let [root (-> data
-                 parse-str
-                 zip/xml-zip)
-        items (zip-xml/xml-> root
-                             :channel
-                             :item)]
-    items))
+(defn- strip-tags [s]
+  (string/replace s #"(<.*?>)|((&lt|&LT|&#x0003C;|&#60;);.*?(&gt;|&GT;|&#x0003E;|&#62;))" ""))
+
+(def formatter (format/formatter "EEE, dd MMM yyyy HH:mm:ss Z"))
+
+(defn- parse-date [s]
+  (format/parse formatter s))
+
+(defn- item-field [item field]
+  (zip-xml/xml1-> item field zip-xml/text))
+
+(defn- get-item [item]
+  (let [title (item-field item :title)
+        description (item-field item :description)
+        date (item-field item :pubDate)]
+    {:text (strip-tags (str title " " description))
+     :date (parse-date date)}))
+
+(defn parse [data]
+  (let [root (-> data parse-str zip/xml-zip)
+        items (zip-xml/xml-> root :channel :item)]
+    (map get-item items)))
--- test/time_slip/feed_test.clj
+++ test/time_slip/feed_test.clj
@@ -1,8 +1,13 @@
 (ns time-slip.feed-test
   (:require [clojure.test :refer :all]
+            [clj-time.core :as time]
             [time-slip.feed :refer :all]))
+
+
 
 (deftest parse-rss
   (testing "parse xml file"
     (let [items (-> "test/rss2sample.xml" slurp parse)]
-      (is (= (count items) 4)))))
+      (is (= (count items) 4))
+      (is (= (-> items first :text) "Star City How do Americans get ready to work with Russians aboard the International Space Station? They take a crash course in culture, language and protocol at Russia's Star City."))
+      (is (= (-> items first :date) (time/date-time 2003 6 3 8 39 21))))))
