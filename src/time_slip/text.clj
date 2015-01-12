bca191591f839dcae132f1df001ee73db5519bf5
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-01-12 09:24:49 +0100

    Add noun-seqs to get groups of nouns

--- src/time_slip/text.clj
+++ src/time_slip/text.clj
@@ -1,16 +1,43 @@
 (ns time-slip.text
   (:require [opennlp.nlp :as nlp]))
 
-
 (def tokenize (nlp/make-tokenizer "models/de-token.bin"))
 (def pos-tag (nlp/make-pos-tagger "models/de-pos-maxent.bin"))
 
-(defn- noun? [[_ pos]]
-  (contains? #{"FM" "NN" "NE"} pos))
-
-(defn get-nouns [s]
+(defn get-words [s]
+  "splits string into seq of word tuples [word, type]"
   (->> s
-      tokenize
-      pos-tag
-      (filter noun?)
-      (map first)))
+       tokenize
+       pos-tag))
+
+(defn- noun? [[_ type]]
+  (contains? #{"FM" "NN" "NE"} type))
+
+(defn- noun=word? [noun [str _]]
+  (= noun str))
+
+(defn- take-last-while [pred col]
+  (->> col
+      reverse
+      (take-while pred)
+      reverse))
+
+(defn- split-by [pred col]
+  (let [head (take-while (comp not pred) col)
+        tail (drop (count head) col)]
+    [head tail]))
+
+(defn noun-seqs
+  "returns all subsequences of nouns containing the noun"
+  ([noun words] (noun-seqs noun [] (split-by #(noun=word? noun %) words)))
+  ([noun seqs [head [word & tail]]]
+     (if (noun=word? noun word)
+       (let [seq (concat (take-last-while noun? head)
+                         [word]
+                         (take-while noun? tail))
+             seqs (concat seqs [seq])]
+         #_(println seq head tail)
+         (if (not (next tail))
+           seqs
+           (recur  noun seqs (split-by #(noun=word? noun %) tail))))
+       seqs)))
--- test/time_slip/text_test.clj
+++ test/time_slip/text_test.clj
@@ -2,10 +2,24 @@
   (:require [clojure.test :refer :all]
             [time-slip.text :refer :all]))
 
-(def test-text "Es sollte eine Premiere werden: Die Trägerrakete des Raumfahrtunternehmens SpaceX sollte nach ihrem Flug nicht wie üblich verglühen, sondern wieder landen. Die Trägerrakete konnte noch eine Raumkapsel zur Internationalen Weltraumstation ISS schicken, stürzte jedoch danach ab.")
 
-(deftest get-nouns-test
-  (testing "nouns"
-    (is (= (get-nouns test-text)
-           ["Premiere" "Trägerrakete" "Raumfahrtunternehmens" "SpaceX"
-            "Flug" "Trägerrakete" "Raumkapsel" "Weltraumstation" "ISS"]))))
+(deftest get-words-test
+    (testing "nouns"
+      (is (= (get-words "Ruft die Feuerwehr!")
+             '(["Ruft" "NN"] ["die" "ART"] ["Feuerwehr" "NN"] ["!" "$."])))))
+
+
+
+(deftest noun-seqs-test
+  (let [words (get-words "Peter Müller mag Frau Sahra Müller und Angie.")]
+    (testing "noun at start of sequence"
+      (is (= (noun-seqs "Peter" words)
+             (list (list ["Peter" "NE"] ["Müller" "NE"])))))
+    (testing "noun in the middle of sequence"
+      (is (= (noun-seqs "Sahra" words)
+             (list (list ["Frau" "NN"] ["Sahra" "NE"] ["Müller" "NE"])))))
+    (testing "noun at the end of sequence"
+      (is (= (noun-seqs "Müller" words)
+             (list
+              (list ["Peter" "NE"] ["Müller" "NE"])
+              (list ["Frau" "NN"] ["Sahra" "NE"] ["Müller" "NE"])))))))
