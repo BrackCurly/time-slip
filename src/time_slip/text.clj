04c1095fa1807260998bf06e5f55e039d031e7c0
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-01-13 14:28:45 +0100

    Replace fancy nlp with regexes

--- src/time_slip/text.clj
+++ src/time_slip/text.clj
@@ -1,21 +1,38 @@
 (ns time-slip.text
-  (:require [opennlp.nlp :as nlp]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
-(def tokenize (nlp/make-tokenizer "models/de-token.bin"))
-(def pos-tag (nlp/make-pos-tagger "models/de-pos-maxent.bin"))
+(def punctuation-tokens #{"." "-" "„" "“" "’" "'" "\"" "/"})
+
+;; match ignored characters or hyphen between spaces
+(def ignore-regex  #"[:,;!?+]|((?<=\s)-(?=\s))")
+
+;; match before or after punctuation char or multiple whitespaces
+(def split-regex  #"(?<=[.\-„“\"’'/])|(?=[.\-„“\"’'/])|\s{1,}")
+
+(defn- punctuation-token? [token]
+  (contains? punctuation-tokens token))
+
+(defn- stop-token? [token]
+  (= token (str/lower-case token)))
+
+(defn- token-type [token]
+  (cond
+   (punctuation-token? token) :punctuation
+   (stop-token? token) :stop
+   :else :noun))
+
+(defn- get-tokens [s]
+  (-> s
+      (str/replace ignore-regex "")
+      (str/split split-regex)))
+
+(defn- get-word [token]
+  [token (token-type token)])
 
 (defn get-words [s]
-  "splits string into seq of word tuples [word, type]"
-  (->> s
-       tokenize
-       pos-tag))
-
-(defn- noun? [[_ type]]
-  (contains? #{"FM" "NN" "NE"} type))
-
-(defn- noun=word? [noun [str _]]
-  (= noun str))
+  "splits string into seq of word tuples [token, type]"
+  (->> (get-tokens s)
+       (map get-word)))
 
 (defn- take-last-while [pred col]
   (->> col
@@ -28,18 +45,25 @@
         tail (drop (count head) col)]
     [head tail]))
 
+(defn- noun? [[_ type]]
+  (= type :noun))
+
+(defn- stop? [[_ type]]
+  (= type :stop))
+
 (defn noun-seqs
-  "returns all subsequences of nouns containing the noun"
-  ([noun words] (noun-seqs noun [] (split-by #(noun=word? noun %) words)))
-  ([noun seqs [head [word & tail]]]
-     (if (noun=word? noun word)
-       (let [seq (concat (take-last-while noun? head)
+  "returns all sequences of nouns containing the token.
+   Seqence may contain punctuation tokens"
+  ([token words] (noun-seqs token [] (split-by #(= (first %) token) words)))
+  ([token seqs [head [word & tail]]]
+     (if (= token (first word))
+       (let [seq (concat (take-last-while #(not (stop? %)) head)
                          [word]
-                         (take-while noun? tail))
+                         (take-while #(not (stop? %)) tail))
              seqs (concat seqs [seq])]
          (if (not (next tail))
            seqs
-           (recur  noun seqs (split-by #(noun=word? noun %) tail))))
+           (recur token seqs (split-by #(= (first %) token) tail))))
        seqs)))
 
 (defn- freq-map [xs]
--- test/time_slip/text_test.clj
+++ test/time_slip/text_test.clj
@@ -3,28 +3,30 @@
             [time-slip.text :refer :all]))
 
 (deftest get-words-test
-    (testing "nouns"
-      (is (= (get-words "Ruft die Feuerwehr!")
-             '(["Ruft" "NN"] ["die" "ART"] ["Feuerwehr" "NN"] ["!" "$."])))))
+    (testing "split string into words stop-words and seperators"
+      (is (= (get-words "Taucher bergen Stimmenrekorder der AirAsia-Maschine")
+             '(["Taucher" :noun] ["bergen" :stop] ["Stimmenrekorder" :noun]
+               ["der" :stop] ["AirAsia" :noun] ["-" :punctuation] ["Maschine" :noun])))))
+
 
 (deftest noun-seqs-test
   (let [words (get-words "Peter Müller mag Frau Sahra Müller und Angie.")]
     (testing "noun at start of sequence"
       (is (= (noun-seqs "Peter" words)
-             (list (list ["Peter" "NE"] ["Müller" "NE"])))))
+             (list [["Peter" :noun]  ["Müller" :noun]]))))
     (testing "noun in the middle of sequence"
       (is (= (noun-seqs "Sahra" words)
-             (list (list ["Frau" "NN"] ["Sahra" "NE"] ["Müller" "NE"])))))
+             (list [["Frau" :noun] ["Sahra" :noun] ["Müller" :noun]]))))
     (testing "noun at the end of sequence"
       (is (= (noun-seqs "Müller" words)
              (list
-              (list ["Peter" "NE"] ["Müller" "NE"])
-              (list ["Frau" "NN"] ["Sahra" "NE"] ["Müller" "NE"])))))))
+              [["Peter" :noun] ["Müller" :noun]]
+              [["Frau" :noun] ["Sahra" :noun] ["Müller" :noun]]))))))
 
 (deftest most-significant-noun-test
   (let [words-single (get-words "Das Schicksal ist im Spiel. Unser Schicksal tragen wir.")
         words-group (get-words "Angela Merkel ist Bundeskanzlerin. Angela Merkel ist eine Frau.")]
         (testing "most significant noun is single word"
-          (is (= (most-significant-noun words-single) "Schicksal")))
+          (is (= (most-significant-noun words-single) "Das Schicksal")))
         (testing "most significant noun is group of words"
           (is (= (most-significant-noun words-group) "Angela Merkel")))))
