61f38669826800d7895f1d17384aee452d9cb86a
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-01-18 16:02:42 +0100

    Print word formated with date and month

--- src/time_slip/core.clj
+++ src/time_slip/core.clj
@@ -1,6 +1,7 @@
 (ns time-slip.core
   (:require [clj-http.client :as client]
             [clj-time.core :as t]
+            [clj-time.predicates :as pr]
             [clj-time.coerce :as coerce]
             [time-slip.text :as txt]
             [time-slip.feed :as f]))
@@ -9,7 +10,8 @@
             "http://www.faz.net/rss/aktuell/"
             "http://newsfeed.zeit.de/news/index"])
 
-(def start-date (coerce/to-long (t/today-at 0 0)))
+(def today  (t/local-date 2014 1 1) #_(t/today-at 0 0))
+(def today-timestamp (coerce/to-long start-date))
 
 (defn parse-res [res]
   (-> res :body f/parse))
@@ -25,12 +27,12 @@
        (take-while (fn [[_ n]] (> n 1)))
        (filter (fn[[s-seq _]] (.contains s-seq s)))))
 
-(defn main []
+(defn fetch-most-freq-noun []
   (let [nouns-freq (->> feeds
                         (map client/get)
                         (filter #(= (:status %) 200))
                         (mapcat parse-res)
-                        (filter #(> (:timestamp %) start-date))
+                        (filter #(>= (:timestamp %) today-timestamp))
                         (map :title)
                         (map nouns-freq)
                         (apply merge-with +)
@@ -45,3 +47,31 @@
       (if (> (/ (second noun) 2) (second noun-seq))
         (first noun)
         (first noun-seq))))))
+
+(defn month-str [month]
+  (str "    " (case month
+                1 "JANUAR"
+                2 "FEBRUAR"
+                3 "MÄRZ"
+                4 "APRIL"
+                5 "MAI"
+                6 "JUNI"
+                7 "JULI"
+                8 "AUGUST"
+                9 "SEPTEMBER"
+                10 "OKOTBER"
+                11 "NOVEMBER"
+                12 "DEZEMBER") "\n\n"))
+
+(defn day-str [day word]
+  (str (if (< day 10) " " "")
+       day ". " (subs word 0 (min 20 (count word)))))
+
+(defn first-day-of-year [date]
+  (and (= (t/day date) 1)
+       (= (t/month date) 1)))
+
+(defn main []
+  (if (pr/first-day-of-month? today)
+    (println (month-str (t/month today))))
+  (println (day-str (t/day today) (fetch-most-freq-noun))))
